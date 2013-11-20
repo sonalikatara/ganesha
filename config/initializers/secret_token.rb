12728c6d44a3313921340e92fb117cfc981c5ddf
sonali <sonalikatara@gmail.com>
2013-11-20 12:17:27 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ganesha::Application.config.secret_key_base = '9a9180f08383a5fc430d88d52439401f8e45789f43fa49d4ead9638761c9bbd6587712fda364e9f217682ed1687cd8972a6cde77200da60ec1b17d0b6957fb8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ganesha::Application.config.session_store :cookie_store, key: '_ganesha_session'
